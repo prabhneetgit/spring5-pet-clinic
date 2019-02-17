eb572b518d7a326a2750925a4a1379d5238391d3
CA\prabhnarora <prabhnarora@deloitte.ca>
2019-02-17 01:17:23 -0500

    refactoring the common curd interface. Closes #11.

--- pet-clinic-data/src/main/java/com/prabhneet/spring/spring5petclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/prabhneet/spring/spring5petclinic/services/PetService.java
@@ -5,11 +5,6 @@ import com.prabhneet.spring.spring5petclinic.model.Pet;
 
 import java.util.Set;
 
-public interface PetService {
+public interface PetService extends CrudService<Pet, Long>{
 
-    Pet findById(Long id);
-
-    Pet save(Pet owner);
-
-    Set<Pet> findAll();
 }
