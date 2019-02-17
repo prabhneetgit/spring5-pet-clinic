6e51b5f755f179d1a963239bede852a9e1219b10
CA\prabhnarora <prabhnarora@deloitte.ca>
2019-02-17 18:10:11 -0500

    added PetTypeService closes #29

---
+++ pet-clinic-data/src/main/java/com/prabhneet/spring/spring5petclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.prabhneet.spring.spring5petclinic.services;
+
+import com.prabhneet.spring.spring5petclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
