ff3a09552dd1cf99bbb0df88a91ff198535b279f
CA\prabhnarora <prabhnarora@deloitte.ca>
2019-02-18 19:18:43 -0500

    Impl find owner. Closes #54

--- pet-clinic-data/src/main/java/com/prabhneet/spring/spring5petclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/prabhneet/spring/spring5petclinic/repositories/OwnerRepository.java
@@ -3,7 +3,11 @@ package com.prabhneet.spring.spring5petclinic.repositories;
 import com.prabhneet.spring.spring5petclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
 
     Owner findByLastName(String lastName);
+
+    List<Owner> findAllByLastNameLike(String lastName);
 }
--- pet-clinic-data/src/main/java/com/prabhneet/spring/spring5petclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/prabhneet/spring/spring5petclinic/services/OwnerService.java
@@ -2,9 +2,12 @@ package com.prabhneet.spring.spring5petclinic.services;
 
 import com.prabhneet.spring.spring5petclinic.model.Owner;
 
+import java.util.List;
 import java.util.Set;
 
 public interface OwnerService extends CrudService<Owner, Long>{
 
     Owner findByLastName(String lastName);
+
+    List<Owner> findAllByLastNameLike(String lastName);
 }
