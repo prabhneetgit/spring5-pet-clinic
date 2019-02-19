2909dc005462f043e4238b47d8aba39debac9f99
CA\prabhnarora <prabhnarora@deloitte.ca>
2019-02-18 23:38:48 -0500

    Fixed find by like. Closes #65

--- pet-clinic-web/src/main/java/com/prabhneet/spring/spring5petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/prabhneet/spring/spring5petclinic/controllers/OwnerController.java
@@ -51,7 +51,7 @@ public class OwnerController {
         }
 
         // find owners by last name
-        List<Owner> results = this.ownerService.findAllByLastNameLike(owner.getLastName());
+        List<Owner> results = this.ownerService.findAllByLastNameLike("%"+ owner.getLastName() + "%");
         if (results.isEmpty()) {
             // no owners found
             result.rejectValue("lastName", "notFound", "not found");
